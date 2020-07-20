2b04a8a5e4d4c0e7b5163a1b6d61a4bcff5111e0
Dr.Frame <remenjuk2010@gmail.com>
2020-07-20 17:21:39 +0300

    20.07.2020 carousel finished

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1000,
-        adaptiveHeight: true,
+        // adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/icons/lefticon.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/righticon.svg"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/icons/righticon.svg"></button>',
+        responsive: [
+            {
+            breakpoint: 992,
+            settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+        ]
       });
   });
