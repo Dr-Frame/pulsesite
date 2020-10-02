3fc362caa9cc5abd97b32f7aa3585d4c0defa631
Vlad Dr.Frame <remenjuk2010@gmail.com>
2020-10-02 10:52:19 +0300

    павп

--- src/js/script.js
+++ src/js/script.js
@@ -16,25 +16,28 @@
       });
   }); */
 
-    const slider = tns({
-        container: '.carousel__inner',
-        items: 1,
-        slideBy: 'page',
-        autoplay: false,
-        controls: false,
-        nav: false
-    });
+const slider = tns({
+  container: '.carousel__inner',
+  items: 1,
+  slideBy: 'page',
+  autoplay: false,
+  controls: false,
+  nav: false
+});
 
-    document.querySelector('.prev').addEventListener ('click', function () {
-        slider.goTo('prev');
-      }) ;
-      document.querySelector('.next').addEventListener ('click', function () {
-        slider.goTo('next');
-      }) ;
-    
+document.querySelector('.prev').addEventListener('click', function () {
+  slider.goTo('prev');
+});
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
 
-    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+(function ($) {
+  $(function () {
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
       $(this)
         .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
         .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('active');
     });
+  });
+})(jQuery);
