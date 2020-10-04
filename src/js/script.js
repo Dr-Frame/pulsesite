a27b8abab2d6d2f0e03ea8d57884102adf8de88d
Vlad Dr.Frame <remenjuk2010@gmail.com>
2020-10-04 12:45:07 +0300

    forms

--- src/js/script.js
+++ src/js/script.js
@@ -21,19 +21,69 @@ $(document).ready(function () {
       .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
   });
 
-function toggleSlide(item) {
-  $(item).each(function(i) {
-    $(this).on('click', function(e) {
-      e.preventDefault();
-      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
-      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+  function toggleSlide(item) {
+    $(item).each(function (i) {
+      $(this).on('click', function (e) {
+        e.preventDefault();
+        $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+        $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+      })
+    });
+  };
+
+  toggleSlide('.catalog-item__link');
+  toggleSlide('.catalog-item__back');
+
+  //modal window
+
+  $('[data-modal=consultation]').on('click', function () {
+    $('.overlay, #consultation').fadeIn();
+  });
+  $('.modal__close').on('click', function () {
+    $('.overlay, #consultation, #order , #thanks').fadeOut();
+  });
+
+  $('.button_mini').each(function (i) {
+    $(this).on('click', function () {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn();
     })
   });
-};
 
-toggleSlide('.catalog-item__link');
-toggleSlide('.catalog-item__back');
-  
+  // валидация форм
 
-
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {0} символов")
+        },
+        phone: "Пожалуйста введите свой телефон",
+        email: {
+          required: "Пожалуйста введите свою почту",
+          email: "Неправильно введен адрес почты name@domain.com"
+        }
+      }
+    });
+  };
+  validateForms('#consultation-form');
+  validateForms('#consultation form');
+  validateForms('#order form');
 });
+
+/* $('.button_submit').on('click', function() {
+  $('#consultation, #order').fadeOut();
+  $('#thanks').fadeIn();
+}); */
